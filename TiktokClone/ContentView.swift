caef21002300491f4b86b8304e5761ecbe44156a
Glymaxus <bastiengiat@gmail.com>
2023-11-11 14:01:13 +0100

    Initial Commit

---
+++ TiktokClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TiktokClone
+//
+//  Created by bastien giat on 11/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TiktokClone/TiktokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokCloneApp.swift
+//  TiktokClone
+//
+//  Created by bastien giat on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TiktokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
